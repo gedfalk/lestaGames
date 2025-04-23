d8b783023daa4f561efb07d08f9459cc0834fdc7
gedfalk <blite.co.stn@gmail.com>
2025-04-23 16:29:31 +0700

    Added file uploading

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -7,9 +7,22 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
+@app.get("/")
+async def upload_form(request: Request):
+    return templates.TemplateResponse("index.html",{"request": request})
+
+@app.post("/upload")
+async def upload_file(request: Request, file: UploadFile = File(...)):
     return templates.TemplateResponse(
-            "index.html",
+        "index.html",
+        {"request": request, "uploaded": True}
+    )
+
+
+# Чисто затестить
+@app.get("/test/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+            "test.html",
             {"request": request, "message": "Тестовое LestaGames"}
     )
